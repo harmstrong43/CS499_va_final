cca0bc6ed1256bc7ce1f4df773e1ba7e18170a17
Hannah Armstrong <harmstrongbusiness@gmail.com>
2022-03-07 22:32:29 -0800

    Added basic HTML + CSS base.

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,11 @@
 <!-- 
-	This is the skeleton code provided by Prof. Minsuk Kahng.
-	Please feel free to revise the existing code.
+	Final Project Svelte App
 -->
+
+<svelte:head>
+	<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+</svelte:head>
+
 <script>
 	import { onMount } from "svelte";
 	import { scaleLinear } from "d3-scale";
@@ -62,68 +66,231 @@
 </script>
 
 <main>
-	<h1>Visual Analytics HW 3</h1>
+	<h1>Visual Analytics Final Project</h1>
+	<h2>Instructor Movement Data Visualization</h2>
 
 	<div id="container">
-		<div id="sidebar" style="width: 450px;">
-			<div id="projection-view" class="view-panel">
-				<div class="view-title">Projection View</div>
-				<svg >
-					
+			<div id="video-view" class="view-panel">
+				<div class="view-title">Instructor Path Player</div>
 
-				</svg>
+				<div class="view-anim">
+					<div id="instructor-select">
+						<p id="instructor-select-title">Selected Instructor:</p>
+						<p id="instructor-select-id">A</p>
+					</div>
+					<div id="instructor-activity">
+						<p id="instructor-activity-word">LECTURING</p>
+					</div>
+
+					<svg>
+						<circle cx="100" cy = "103" r="30" fill="#A9B3C6"/>
+						<circle cx="135" cy = "120" r="30" fill="#A9B3C6"/>
+						<circle cx="104" cy = "92" r="30" fill="#A9B3C6"/>
+						<circle cx="159" cy = "104" r="30" fill="#A9B3C6"/>
+					</svg>
+				</div>
+
+				<div class="view-underbar">
+					<div class="view-icons">
+						<i class="fas fa-fast-backward"id="i-back"></i>
+						<i class="fas fa-step-backward"id="i-backstep"></i>
+						<i class="fas fa-play"id="i-play"></i>
+						<i class="fas fa-step-forward"id="i-forwardstep"></i>
+						<i class="fas fa-fast-forward"id="i-forward"></i>
+					</div>
+				</div>
 			</div>
-			<div id="selected-image-view" class="view-panel">
-				<div class="view-title">Selected Image</div>
-				<div id="selected-image-view-content">
-
+			<div id="chart-view" class="view-panel">
+				<div class="view-title">Instructor Statistics</div>
+				<div id="view-list">
+					<p id="stat0">Instructor X spent the most time at location: LOC</p>
+					<p id="stat1">Instructor X spent the most time doing this activity: ACT</p>
+					<p id="stat2">Instructor X traveled this maximum distance away from the original location: DIST</p>
+					<p id="stat3">Continues...</p>
 
 				</div>
 			</div>
-		</div>
-
-		<div id="main-section" style="width: 1000px;">
-			<div id="score-distributions-view" class="view-panel">
-				<div class="view-title">Score Distributions</div>
-				<svg >
-
-
-				</svg>
-			</div>
-		</div>
 	</div>
 </main>
 
 <style>
+
+/*
+	COLORS:
+
+	TITLE BKG (gray-purple): #B6B3BD;
+	BUTTON BKG (gray-green): #B5BDB3;
+	BUTTON NKG 2 (gray-blue): #A9B3C6;
+
+	LIGHT BORDER: #D4D3D9;
+	DARK BORDER: #615f6d;
+*/
+
+/*
+	MAIN DOCUMENT
+*/
+	:global(body)
+	{
+		background-color: #EAE9EC;
+	}
 	h1 {
-		font-size: 1.3rem;
+		font-size: 2rem;
 		font-weight: 500;
 		margin-top: 0;
+		width:100%;
+		text-align: center;
+		margin:5px;
 	}
+	h2
+	{
+		font-size: 1.2rem;
+		font-weight: 500;
+		width:100%;
+		text-align: center;
+		margin:2px;
+	}
+
+/*
+	VIDEO VIEW MAIN
+*/
 	#container {
 		display: flex;
-	}
-	#sidebar, #main-section {
-		display: flex;
-		flex-direction: column;
+		width:auto;
 	}
 	.view-panel {
-		border: 2px solid #eee;
-		margin-bottom: 15px;
-		margin-right: 15px;
+		border: 2px solid #D4D3D9;
+		margin:15px;
+		position:relative;
+		height:600px;
 	}
 	.view-title {
-		background-color: #f3f3f3;
-		font-size: 1.0rem;
+		background-color: #B6B3BD;
+		font-size: 1.2rem;
 		margin-bottom: 8px;
 		padding: 3px 8px 5px 12px;
 	}
-	#selected-image-view-content {
-		height: 100px;
-		padding: 15px;
+	.view-anim
+	{
+		position:relative;
+		padding:5px;
 	}
-	svg {
-		margin: 5px;
+	#video-view
+	{
+		width:70%;
+	}
+	circle
+	{
+		opacity:0.3;
+	}
+
+/*
+	VIDEO VIEW SELECTED INSTRUCTOR
+*/
+	#instructor-select
+	{
+		background-color:#B5BDB3;
+		border: 2px solid #D4D3D9;
+		border-radius: 15px;
+		width:140px;
+		height:70px;
+		position:absolute;
+		margin:0;
+		right:20px;
+		padding-top:5px;
+	}
+	#instructor-select-title
+	{
+		margin:0;
+		padding:0;
+		font-size: 15px;
+		border-bottom:2px solid #615f6d;
+	}
+	#instructor-select p
+	{
+		text-align: center;
+		font-weight: 500;
+
+	}
+	#instructor-select-id
+	{
+		font-size: 30px;
+		margin-top: 5px;
+		position:relative;
+		margin:0;
+	}
+
+/* 
+	VIDEO VIEW INSTRUCTOR ACTIVITY
+*/
+	#instructor-activity
+	{
+		background-color:#a9b3c6;
+		border: 2px solid #D4D3D9;
+		border-radius: 15px;
+		width:100px;
+		height:70px;
+		position:absolute;
+		margin:0;
+		right:180px;
+		padding-top:0;
+		display:flex;
+	}
+	#instructor-activity-word
+	{
+		text-align: center;
+		font-weight: 500;
+		position:relative;
+		margin:0;
+		display:flex;
+		width:100%;
+		align-items:center;
+		justify-content:center;
+		font-size:17px;
+	}
+
+/*
+	VIDEO VIEW UNDERBAR
+*/
+	.view-underbar
+	{
+		position:absolute;
+		bottom:0;
+		width:100%;
+		background-color:#B6B3BD;
+	}
+	.view-icons
+	{
+		display:flex;
+		justify-content:center;
+		padding:5px;
+	}
+
+/* 
+	CHART VIEW MAIN
+*/
+	#chart-view
+	{
+		width:50%;
+	}
+	.fas
+	{
+		font-size:50px;
+		margin-top:5px;
+		margin-bottom:5px;
+		margin-left:15px;
+		margin-right:15x;
+	}
+	#view-list
+	{
+		padding:10px;
+		padding-top:2px;
+
+	}
+	#view-list p
+	{
+		margin:15px;
+		text-align: left;
+		display:block;
 	}
 
 
